ee70cfe52036e20e8fffa7298ee3b9961fa2b853
Andrew Spott <andrew.spott@gmail.com>
2012-11-25 15:21:09 -0700

    back to the different e-field

--- propagate/cranknicholson.hpp
+++ propagate/cranknicholson.hpp
@@ -65,8 +65,8 @@ solve(Vec *wf, context* cntx, Mat *A)
 
     PetscScalar     cn_factor = -std::complex<double>(0, 0.5 * cntx->laser->dt());
     PetscReal       t = 0;
-    PetscScalar     ef = cntx->laser->efield(t)  + cntx->laser->efield(t+cntx->laser->cycles());  //average between the points
-    ef /= 2;
+    PetscScalar     ef = cntx->laser->efield(t); // + cntx->laser->efield(t+cntx->laser->cycles());  //average between the points
+    //ef /= 2;
     PetscReal       maxtime = math::PI * cntx->laser->cycles() / cntx->laser->frequency();
     PetscInt        step = 0;
     Vec             tmp;
@@ -99,23 +99,23 @@ solve(Vec *wf, context* cntx, Mat *A)
         MatCopy(*( cntx->D ), *A , SAME_NONZERO_PATTERN);   // A = D
 
         //This has the same "t" at both sides of the equation... should be different...
-        MatScale(*A, ef);                                   // A = ef(t) * D
-        MatDiagonalSet(*A, *(cntx->H), INSERT_VALUES);      // A = ef(t) * D + H_0
-        MatScale(*A, cn_factor);                            // A = .5 * dt [ ef(t) * D + H_0 ]
-        MatShift(*A, std::complex<double>(1,0));            // A = .5 * dt [ ef(t) * D + H_0 ] + 1
-        MatMult(*A, *wf, tmp);                              // A u_n = tmp
-        MatScale(*A, std::complex<double>(-1,0));           // A = - .5 dt [ef(t+dt) * D + H_0 ] - 1
-        MatShift(*A, std::complex<double>(2,0));            // A = - .5 dt [ef(t+dt) * D + H_0 ] + 1
-        
-        //This has different 't's on both sides:
         //MatScale(*A, ef);                                   // A = ef(t) * D
         //MatDiagonalSet(*A, *(cntx->H), INSERT_VALUES);      // A = ef(t) * D + H_0
         //MatScale(*A, cn_factor);                            // A = .5 * dt [ ef(t) * D + H_0 ]
         //MatShift(*A, std::complex<double>(1,0));            // A = .5 * dt [ ef(t) * D + H_0 ] + 1
         //MatMult(*A, *wf, tmp);                              // A u_n = tmp
-        //MatAXPY(*A, cn_factor * (cntx->laser->efield(t+cntx->laser->dt()) - ef), *( cntx->D ), SAME_NONZERO_PATTERN);
         //MatScale(*A, std::complex<double>(-1,0));           // A = - .5 dt [ef(t+dt) * D + H_0 ] - 1
         //MatShift(*A, std::complex<double>(2,0));            // A = - .5 dt [ef(t+dt) * D + H_0 ] + 1
+        
+        //This has different 't's on both sides:
+        MatScale(*A, ef);                                   // A = ef(t) * D
+        MatDiagonalSet(*A, *(cntx->H), INSERT_VALUES);      // A = ef(t) * D + H_0
+        MatScale(*A, cn_factor);                            // A = .5 * dt [ ef(t) * D + H_0 ]
+        MatShift(*A, std::complex<double>(1,0));            // A = .5 * dt [ ef(t) * D + H_0 ] + 1
+        MatMult(*A, *wf, tmp);                              // A u_n = tmp
+        MatAXPY(*A, cn_factor * (cntx->laser->efield(t+cntx->laser->dt()) - ef), *( cntx->D ), SAME_NONZERO_PATTERN);
+        MatScale(*A, std::complex<double>(-1,0));           // A = - .5 dt [ef(t+dt) * D + H_0 ] - 1
+        MatShift(*A, std::complex<double>(2,0));            // A = - .5 dt [ef(t+dt) * D + H_0 ] + 1
 
         KSPSetOperators(ksp, *A, *A, SAME_NONZERO_PATTERN); // Solve[ A x = tmp ] for x
         KSPSetFromOptions(ksp);
@@ -125,12 +125,14 @@ solve(Vec *wf, context* cntx, Mat *A)
         VecPointwiseMult(*wf, *wf, abs);
 
         t += cntx->laser->dt();
-        ef = cntx->laser->efield(t)  + cntx->laser->efield(t+cntx->laser->cycles());  //average between the points
-        ef /= 2;
+        ef = cntx->laser->efield(t); //  + cntx->laser->efield(t+cntx->laser->cycles());  //average between the points
+        //ef /= 2;
         step++;
         //at the zero of the field: write out the vector:
         if (( ef.real() <= 0. && cntx->laser->efield(t - cntx->laser->dt()).real() > 0 ) || ( ef.real() >= 0 && cntx->laser->efield(t - cntx->laser->dt()).real() < 0 ))
         {
+            if (cntx->hparams->rank() == 0)
+                std::cout << "time: " << t << " step: " << step << " efield: " << ef << " norm-1: " << norm-1 << " *" << std::endl;
             std::ostringstream wf_name;
             wf_name << "./wf_" << zero << ".dat";
             PetscViewerASCIIOpen(cntx->hparams->comm(),wf_name.str().c_str(),&view);
@@ -139,7 +141,7 @@ solve(Vec *wf, context* cntx, Mat *A)
             zero++;
         }
 
-        if (!(step%10))
+        if (!(step%50))
         {
             VecCopy(*wf, prob);
             VecAbs(prob);
@@ -154,7 +156,7 @@ solve(Vec *wf, context* cntx, Mat *A)
                 //std::cerr << "time: " << t << " step: " << step << " efield: " << ef << " norm-1: " << norm-1 << std::endl;
         }
     }
-    file_name = std::string("./final_wf.dat");
+    file_name = std::string("./wf_final.dat");
     PetscViewerASCIIOpen(cntx->hparams->comm(),file_name.c_str(),&view);
     PetscViewerSetFormat(view, PETSC_VIEWER_ASCII_SYMMODU);
     VecView(*wf,view);
