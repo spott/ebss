a3fb40a692bf7735b962d94ff4e3057dfe9a518e
Andrew Spott <andrew.spott@gmail.com>
2013-05-06 15:36:39 -0600

    sent future

--- findbasis/numerov.hpp
+++ findbasis/numerov.hpp
@@ -504,8 +504,9 @@ namespace numerov
                 std::cerr << "=========================================" << std::endl;
                 if ( tmp.n == tmp.l+2 && tmp.j == ((tmp.l>0)? 2 * tmp.l - 1 : 1))
                 {
-                    std::cout << std::this_thread::get_id() << "sending future" << std::endl;
+                    std::cout << "[" << std::this_thread::get_id() << "] sending future" << std::endl;
                     future_guess.set_value( tmp );
+                    std::cout << "[" << std::this_thread::get_id() << "] sent future" << std::endl;
                 }
             }
         }
